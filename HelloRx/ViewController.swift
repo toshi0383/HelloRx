c7c726e2c5fac79c39c6f494e5c2fa741e160c41
toshi0383 <t.suzuki326@gmail.com>
2017-04-01 23:16:57 +0900

    Add sample code

--- HelloRx/ViewController.swift
+++ HelloRx/ViewController.swift
@@ -7,19 +7,101 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    func create() -> Observable<Int> {
+        return .create { observer in
+            print("run")
+            observer.onNext(100)
+            observer.onNext(200)
+            observer.onNext(300)
+            observer.onNext(400)
+            observer.onNext(500)
+            observer.onCompleted()
+            return Disposables.create()
+        }
+    }
+    func log<T>(_ identifier: String) -> (Event<T>) -> () {
+        return { event in
+            switch event {
+            case let .next(value):
+                print("[\(identifier)]", "NEXT", value)
+            case .error:
+                print("[\(identifier)]", "ERROR")
+            case .completed:
+                print("[\(identifier)]", "COMPLETED")
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        test01()
+        test02()
+        test03()
+        test04()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func test01() {
+
+        let o1 = create()
+
+        do {
+            let replaySubject = ReplaySubject<Int>.createUnbounded()
+            _ = o1.bindTo(replaySubject)
+            print("bindTo replaySubject")
+
+            _ = replaySubject.subscribe(log("1"))
+            _ = replaySubject.subscribe(log("2"))
+            _ = replaySubject.subscribe(log("3"))
+            _ = replaySubject.subscribe(log("4"))
+        }
     }
 
+    func test02() {
 
+        let o2 = create()
+        do {
+            let shared = o2.shareReplayLatestWhileConnected()
+            print("shared")
+
+            _ = shared.subscribe(log("a"))
+            _ = shared.subscribe(log("b"))
+            _ = shared.subscribe(log("c"))
+            _ = shared.subscribe(log("d"))
+        }
+    }
+
+    func test03() {
+
+        let o3 = create()
+        do {
+            let shared = o3.shareReplay(3)
+            print("shared")
+
+            _ = shared.subscribe(log("10"))
+            _ = shared.subscribe(log("20"))
+            _ = shared.subscribe(log("30"))
+            _ = shared.subscribe(log("40"))
+        }
+    }
+
+    func test04() {
+
+        let o4 = create()
+        do {
+            let shared = o4.share()
+            print("shared")
+
+            _ = shared.subscribe(log("A"))
+            _ = shared.subscribe(log("B"))
+            _ = shared.subscribe(log("C"))
+            _ = shared.subscribe(log("D"))
+            _ = shared.subscribe(log("E"))
+        }
+    }
 }
 
--- RxPlayground.playground/Contents.swift
+++ RxPlayground.playground/Contents.swift
@@ -4,3 +4,71 @@ import UIKit
 import RxSwift
 
 
+import RxSwift
+
+let o = Observable<Int>.create { observer in
+    print("run")
+    observer.onNext(100)
+    observer.onNext(200)
+    observer.onCompleted()
+
+    return Disposables.create()
+}
+
+func log<T>(_ identifier: String) -> (Event<T>) -> () {
+    return { event in
+        switch event {
+        case let .next(value):
+            print("[\(identifier)]", "NEXT", value)
+        case .error:
+            print("[\(identifier)]", "ERROR")
+        case .completed:
+            print("[\(identifier)]", "COMPLETED")
+        }
+    }
+}
+
+do {
+    let shared = o.shareReplay(3)
+    print("shared")
+
+    shared.subscribe(log("1"))
+    shared.subscribe(log("2"))
+    shared.subscribe(log("3"))
+}
+/*
+ shared
+ run
+ [1] NEXT 100
+ [1] NEXT 200
+ [1] COMPLETED
+ [2] NEXT 100
+ [2] NEXT 200
+ [2] COMPLETED
+ run
+ [3] NEXT 100
+ [3] NEXT 200
+ [3] COMPLETED
+ run
+ */
+
+
+do {
+    let shared = o.shareReplay(1)
+    print("shared")
+
+    shared.subscribe(log("1"))
+    shared.subscribe(log("2"))
+    shared.subscribe(log("3"))
+}
+/*
+ shared
+ run
+ [1] NEXT 100
+ [1] NEXT 200
+ [1] COMPLETED
+ [2] NEXT 200
+ [2] COMPLETED
+ [3] NEXT 200
+ [3] COMPLETED
+ */
