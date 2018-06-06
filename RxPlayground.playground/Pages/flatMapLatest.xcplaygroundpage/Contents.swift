aecc34e29c0411cff7c63d574c2e79f30c959409
toshi0383 <t.suzuki326@gmail.com>
2018-06-06 11:46:55 +0900

    Update contents with Xcode9.3.1

--- RxPlayground.playground/Pages/flatMapLatest.xcplaygroundpage/Contents.swift
+++ RxPlayground.playground/Pages/flatMapLatest.xcplaygroundpage/Contents.swift
@@ -37,20 +37,38 @@ func _log<T>(_ identifier: String) -> (Event<T>) -> () {
     }
 }
 
-let firstSlowResponse = create(interval: 1)
-let secondFastResponse = create(interval: 0.2)
+func test01() {
+    let firstSlowResponse = create(interval: 1)
+    let secondFastResponse = create(interval: 0.2)
 
-_ = Observable<Int>.timer(0, period: RxTimeInterval(exactly: 3), scheduler: MainScheduler.instance)
-    .take(2)
-    .flatMapLatest { num -> Observable<String> in
-        if num % 2 == 0 {
-            return firstSlowResponse.debug("[firstSlowResponse]")
-        } else {
-            // trigger firstSlowResponse to get disposed
-            return secondFastResponse
+    _ = Observable<Int>.timer(0, period: RxTimeInterval(exactly: 3), scheduler: MainScheduler.instance)
+        .take(2)
+        .flatMapLatest { num -> Observable<String> in
+            if num % 2 == 0 {
+                return firstSlowResponse.debug("[firstSlowResponse]")
+            } else {
+                // trigger firstSlowResponse to get disposed
+                return secondFastResponse
+            }
         }
-    }
-    .subscribe(_log("hello"))
+        .subscribe(_log("hello"))
+}
+
+func test02() {
+    let slowResponse = create(interval: 0.7)
+    let fastResponse = create(interval: 0.2)
+
+    _ = //Observable<Int>.timer(0, period: RxTimeInterval(exactly: 0.5), scheduler: MainScheduler.instance)
+        slowResponse
+        .take(2)
+        .flatMapLatest { num -> Observable<String> in
+            return fastResponse.debug("[fastResponse]")
+        }
+        .sample(slowResponse.map { _ in }.debug("[sampler]"))
+        .subscribe(_log("hello"))
+}
+
+test02()
 
 RunLoop.main.run(until: Date(timeIntervalSinceNow: 14))
 
